edb8cbd8a38ed52a970b2976e2a68c58b20bec39
Harish <harish@ET-LV-LAP-0027.local>
2020-12-21 19:15:58 +0530

    Commit in test branch

--- KioskTest/android/test.js
+++ KioskTest/android/test.js
@@ -5,3 +5,6 @@
 // Added one more line in test file
 
 //Test ateasfdds asd
+
+//I am in test branch 2
+//lsfljlasfl;dksjflksldakfj;lsjfljsdl;jds;afklasjflkjdsalkfj;lkasdjfdsjaf;jldsa;fjdslafjl;das;fljas;ldfjl;asdjflkjsak;fldsafjlsdajlf;sdlafjaslfjl;sdaasjflds;fj
